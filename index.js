a478fa3e9c4c2fe9506ee4cf7e7ac4333c540fa6
ban hussein <banhussien97@gmail.com>
2019-10-15 22:42:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=function(array){
+  return array.slice(0,2) }
+  
+  const returnLastTwoDrivers=function(array){
+    return array.slice(-2)
+  }
+  
+  let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+  
+  const createFareMultiplier=function (number){
+    return function(fare){
+      
+    
+    return number*fare
+  }
+  }
+  const fareDoubler=createFareMultiplier(2);
+const fareTripler=createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,selector){
+   if(selector == returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+  }
+  else{
+     return returnLastTwoDrivers(drivers);
+  }
+
+}
